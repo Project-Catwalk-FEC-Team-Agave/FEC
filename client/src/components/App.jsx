14891d5ef5bdd21ba615039a5f7e3a470b7779b8
Thomas Wu <thomas.wu95@gmail.com>
2021-07-21 12:45:06 -0700

    changed Q&A to questions and added behavior in App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+// import Overview from './Overview/';
+import QA from './questions/qa.jsx';
+
 
 
 class App extends React.Component {
 
 
+  render() {
 
+    return (
+    // render navbar
+    // render overview
+    // render related
+    // renter Q&A
+    // render reivews
+    <div>
+      <QA />
+    </div>
+
+    )
+
+  }
 }
-
 export default App;
---
+++ client/src/components/questions/qa.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class QA extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+
+}
+
+export default QA;
