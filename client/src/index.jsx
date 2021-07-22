17ab2c347e62226f4e66925991e8dafc414b55be
Cam Boucher <cam.boucher.28@gmail.com>
2021-07-22 02:24:23 -0400

    Minor adjustment to npm run and build scripts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import Overview from './components/Overview/overview.jsx';
+import QA from './components/questions/qa.jsx';
+import Related from './components/Related Items/related.jsx';
+import Reviews from './components/Reviews/reviews.jsx'
 
 ReactDOM.render(
   <App />,
