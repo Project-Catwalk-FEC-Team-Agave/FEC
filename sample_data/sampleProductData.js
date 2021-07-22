cfcef3bccd3840e3b1c9e562437734c1cb83211e
Cam Boucher <cam.boucher.28@gmail.com>
2021-07-22 04:23:38 -0400

    Added sample data for reviews and reviews/meta

--- sample_data/sampleProductData.js
+++ sample_data/sampleProductData.js
@@ -21,3 +21,66 @@ const sampleProductData = {
 };
 
 export default sampleProductData;
+
+/*
+Sample Review Data (HTTP get request '/reviews?product_id=2')
+
+{
+  "product": "2",
+  "page": 0,
+  "count": 5,
+  "results": [
+    {
+      "review_id": 5,
+      "rating": 3,
+      "summary": "I'm enjoying wearing these shades",
+      "recommend": false,
+      "response": null,
+      "body": "Comfortable and practical.",
+      "date": "2019-04-14T00:00:00.000Z",
+      "reviewer_name": "shortandsweeet",
+      "helpfulness": 5,
+      "photos": [{
+          "id": 1,
+          "url": "urlplaceholder/review_5_photo_number_1.jpg"
+        },
+        {
+          "id": 2,
+          "url": "urlplaceholder/review_5_photo_number_2.jpg"
+        },
+        // ...
+      ]
+    }
+  ]
+}
+
+Sample Review Metadata (HTTP get request '/reviews/meta?product_id=2)
+{
+  "product_id": "2",
+  "ratings": {
+    2: 1,
+    3: 1,
+    4: 2,
+    // ...
+  },
+  "recommended": {
+    0: 5
+    // ...
+  },
+  "characteristics": {
+    "Size": {
+      "id": 14,
+      "value": "4.0000"
+    },
+    "Width": {
+      "id": 15,
+      "value": "3.5000"
+    },
+    "Comfort": {
+      "id": 16,
+      "value": "4.0000"
+    },
+    // ...
+}
+
+*/
