58fc9cf6861e92bfb43416c89d2d54f034de3581
Cam Boucher <cam.boucher.28@gmail.com>
2021-07-28 00:06:43 -0400

    Fixed conflicts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import Overview from './components/Overview/overview.jsx';
 import QA from './components/questions/qa.jsx';
-<<<<<<< HEAD
-import Related from './components/Related Items/related.jsx';
 import Reviews from './components/Reviews/Reviews.jsx'
-=======
 import Related from './components/Related Items/RelatedProductsContainer.jsx';
-import Reviews from './components/Reviews/reviews.jsx'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
->>>>>>> cbdbc8c2619adc493e9393cddd8b22f6c93452ac
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// )
 
 // ReactDOM.render(
 //   <App />,
