669ad88901c74e7c9bcfb2c22c1c2b5cbe4a1c95
Erin Grisham <erinmgrisham@gmail.com>
2021-08-01 17:57:49 -0400

    Fixed card height

--- client/src/components/Related Items/Related Products Carousel/Related Products Card/styles.js
+++ client/src/components/Related Items/Related Products Carousel/Related Products Card/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   root: {
     maxWidth: 245,
+    maxHeight: 400,
 		marginBottom: '20px'
   },
   media: {
