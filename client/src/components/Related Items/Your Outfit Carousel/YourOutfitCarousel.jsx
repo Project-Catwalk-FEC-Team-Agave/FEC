2e405b4a2344293fc8358f496ee50794891c192d
Erin Grisham <erinmgrisham@gmail.com>
2021-08-04 11:32:00 -0400

    closer to rendering out Your Outfit cards

--- client/src/components/Related Items/Your Outfit Carousel/YourOutfitCarousel.jsx
+++ client/src/components/Related Items/Your Outfit Carousel/YourOutfitCarousel.jsx
@@ -6,6 +6,7 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 import { IconButton } from '@material-ui/core';
 import useStyles from './styles.js';
+import { Card, CardMedia} from '@material-ui/core';
 
 
 const YourOutfitCarousel = ({ productInfo, photoObjs, relatedProductsIDs, reviewsData }) => {
@@ -20,31 +21,57 @@ const YourOutfitCarousel = ({ productInfo, photoObjs, relatedProductsIDs, review
     //     return 'https://images.unsplash.com/photo-1492447105260-2e947425b5cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80';
     //   })
     // }
-    let choosePhoto = (i) => {
-      return (i === 0 ? 'https://media.istockphoto.com/photos/dark-gray-background-picture-id511017202?k=6&m=511017202&s=170667a&w=0&h=0FlZ5CYuDYv68CmuO5R7EGQN7DMrobuu5AfX6g3J84Q=' : 'https://images.unsplash.com/photo-1492447105260-2e947425b5cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
-    }
-
-  return (
-      <Grid className={classes.mainContainer} container alignItems='stretch' spacing={4}>
-    <div className="carousel">
-			<div className="innerCarousel">
-			    {productInfo.map((product, i) => (
-				  <Grid key={i} xs={12} sm={6} item>
-            <YourOutfitCard
-              photo={choosePhoto(i)}
+    let renderCards = (product, i) => {
+      if (i === 0) {
+        return (
+          <Card
+            className={classes.root}>
+            <CardMedia
+              className={classes.media}
+              image={'https://media.istockphoto.com/photos/dark-gray-background-picture-id511017202?k=6&m=511017202&s=170667a&w=0&h=0FlZ5CYuDYv68CmuO5R7EGQN7DMrobuu5AfX6g3J84Q='}
+            >
+              <IconButton style={{ color: 'black' }} aria-label="settings" className={classes.overlay}>
+                {/* <HighlightOffIcon style={{ color: 'black' }} aria-label="delete item from Your Outfits"/> */}
+              </IconButton>
+            </CardMedia>
+          </Card>
+        );
+      } else {
+        return (
+          <YourOutfitCard
+              photo={'https://media.istockphoto.com/photos/dark-gray-background-picture-id511017202?k=6&m=511017202&s=170667a&w=0&h=0FlZ5CYuDYv68CmuO5R7EGQN7DMrobuu5AfX6g3J84Q='}
               product={product}
               reviewsData={reviewsData}>
 
-            </YourOutfitCard>
-				  </Grid>
-			    ))}
-				<div className="carouselRight">
-				<IconButton style={{ color: 'black' }} aria-label="settings" className={classes.overlay}>
-          <ArrowForwardIosIcon style={{ color: 'black' }} aria-label="show next item in related products list"/>
-        </IconButton>
-				</div>
-			</div>
-    </div>
+          </YourOutfitCard>
+        )
+
+      }
+      // return (i === 0 ? 'https://media.istockphoto.com/photos/dark-gray-background-picture-id511017202?k=6&m=511017202&s=170667a&w=0&h=0FlZ5CYuDYv68CmuO5R7EGQN7DMrobuu5AfX6g3J84Q=' : photoObjs[i].photo);
+    }
+
+  return (
+    <Grid className={classes.mainContainer} container alignItems='stretch' spacing={4}>
+      <div className="carousel">
+        <div className="innerCarousel">
+            {productInfo.map((product, i) => (
+            <Grid key={i} xs={12} sm={6} item>
+              {renderCards(product, i)}
+              {/* <YourOutfitCard
+                photo={choosePhoto(i)}
+                product={product}
+                reviewsData={reviewsData}>
+
+              </YourOutfitCard> */}
+            </Grid>
+            ))}
+          <div className="carouselRight">
+          <IconButton style={{ color: 'black' }} aria-label="settings" className={classes.overlay}>
+            <ArrowForwardIosIcon style={{ color: 'black' }} aria-label="show next item in related products list"/>
+          </IconButton>
+          </div>
+        </div>
+      </div>
 		</Grid>
   )
 }
--- client/src/components/Related Items/Your Outfit Carousel/styles.js
+++ client/src/components/Related Items/Your Outfit Carousel/styles.js
@@ -5,5 +5,21 @@ export default makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
 		paddingTop: '50px'
+  },
+  root: {
+    maxWidth: 245,
+    height: 450,
+    marginTop: '20px',
+		marginBottom: '20px',
+    marginRight: '10px',
+    marginLeft: '10px'
+  },
+  media: {
+    paddingTop: '100%'
+  },
+	overlay: {
+		position: 'absolute',
+		top: '20px',
+		left: '195px'
   }
 }));
