959a7a5fefaabcc94f88c409bfdfcd4c3b6388fe
Erin Grisham <erinmgrisham@gmail.com>
2021-08-04 11:09:51 -0400

    changed first Your Outfit card photo to gray bg

--- client/src/components/Related Items/Your Outfit Carousel/YourOutfitCarousel.jsx
+++ client/src/components/Related Items/Your Outfit Carousel/YourOutfitCarousel.jsx
@@ -12,13 +12,16 @@ const YourOutfitCarousel = ({ productInfo, photoObjs, relatedProductsIDs, review
 
     const classes = useStyles();
 
-    let choosePhoto = (prod) => {
-      return photoObjs.map(obj => {
-        if (obj.productID === prod.id) {
-          return obj.photo;
-        }
-        return 'https://images.unsplash.com/photo-1492447105260-2e947425b5cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80';
-      })
+    // let choosePhoto = (prod) => {
+    //   return photoObjs.map(obj => {
+    //     if (obj.productID === prod.id) {
+    //       return obj.photo;
+    //     }
+    //     return 'https://images.unsplash.com/photo-1492447105260-2e947425b5cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80';
+    //   })
+    // }
+    let choosePhoto = (i) => {
+      return (i === 0 ? 'https://media.istockphoto.com/photos/dark-gray-background-picture-id511017202?k=6&m=511017202&s=170667a&w=0&h=0FlZ5CYuDYv68CmuO5R7EGQN7DMrobuu5AfX6g3J84Q=' : 'https://images.unsplash.com/photo-1492447105260-2e947425b5cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
     }
 
   return (
@@ -27,10 +30,12 @@ const YourOutfitCarousel = ({ productInfo, photoObjs, relatedProductsIDs, review
 			<div className="innerCarousel">
 			    {productInfo.map((product, i) => (
 				  <Grid key={i} xs={12} sm={6} item>
-					<YourOutfitCard
-						photo={choosePhoto(product)}
-						product={product}
-						reviewsData={reviewsData}/>
+            <YourOutfitCard
+              photo={choosePhoto(i)}
+              product={product}
+              reviewsData={reviewsData}>
+
+            </YourOutfitCard>
 				  </Grid>
 			    ))}
 				<div className="carouselRight">
